444577e680d7660260d219282cc5e65614d7bc9e
User <User@5114001660TI778>
2022-10-16 19:29:06 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 4; j++)
+                {
+                    Console.Write("+");
+                }
+                for (int x = 0; x <= i; x++)
+                {
+                    Console.Write(i + 1);
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
